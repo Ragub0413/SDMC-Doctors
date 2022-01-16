8e12a62221a25156d798dc380001e912f4fbfe3b
Ragub0413 <ragubangelica13@gmail.com>
2022-01-16 13:10:36 +0800

    first

--- src/Components/Connection/Action/appointments.js
+++ src/Components/Connection/Action/appointments.js
@@ -1,2 +1,14 @@
 import { FETCH_ALL,CREATE,UPDATE } from '../Constant/action.Type.js'
 import * as api from '../Api/index.js';
+export const cancelAppointment =(id,cancelApps) => async (dispatch) =>{
+    try{
+        const {data} = await api.cancelAppointment(id, cancelApps)
+
+        dispatch({type: 'UPDATE', payload: data})
+
+        console.log("di ko alam kung tama pa to");
+
+    }catch(error){
+        console.log(error.message);
+    }
+}
